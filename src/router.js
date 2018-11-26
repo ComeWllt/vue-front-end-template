976c4af8f28509b770d2b48045530b341a245417
come.willot <come.willot@thinxtra.com>
2018-11-26 15:15:26 +1100

    Add router scroll behaviour

--- src/router.js
+++ src/router.js
@@ -20,4 +20,10 @@ export default new Router({
       component: ExampleView,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  },
 });
